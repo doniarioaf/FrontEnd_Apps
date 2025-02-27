fd913fff4db05e076ba25c2888fd59850f9890af
donisimpoool <doni.ario@simpool.id>
2025-02-27 14:18:40 +0700

    no message

--- src/containers/shared/apiURL.js
+++ src/containers/shared/apiURL.js
@@ -4,7 +4,6 @@ export const baseApiURL =
     // 'http://103.190.29.182:8080/manggala/v1';
     // 'http://localhost:8189/manggala/v1'; //Doni
     'http://localhost:8080/v1';
-    // 'http://147.139.139.25:80/sinarmediasakti/v1';
 export const loginURL = `${baseApiURL}/login`;
 export const checkAuthURL = `${baseApiURL}/checkuser`;
 export const baseBranchURL = param => `${baseApiURL}/branch${param}`;
