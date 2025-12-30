1d06e5023b6d04e59832a4236f4386ca2cbbb6a3
donisimpoool <doni.ario@simpool.id>
2025-12-30 20:15:53 +0700

    Pelunasan Hutang di tab Hutang dan tab Pelunasan Hutang , di view dalam , dimunculkan tanggal dokumen

--- src/containers/Transaksi/PelunasanHutang/detailpelunasanhutang.js
+++ src/containers/Transaksi/PelunasanHutang/detailpelunasanhutang.js
@@ -443,6 +443,13 @@ import React, {useState,
                             </strong>
                             </div> 
 
+                            <div className="row mt-3">
+                            <span className="col-md-5">{i18n.t('Tanggal')}</span>
+                                <strong className="col-md-7">
+                                {ValueDetailPelunasan.date ?moment (new Date(ValueDetailPelunasan.date)).format(formatdate):''}
+                                </strong>
+                            </div>
+
                             {
                                 IsPR?
                                     (
