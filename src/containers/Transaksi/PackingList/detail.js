5e23091de7e85e7f151efb6601f94d502b80d9fe
donisimpoool <doni.ario@simpool.id>
2026-06-12 21:45:49 +0700

    no message

--- src/containers/Transaksi/PackingList/detail.js
+++ src/containers/Transaksi/PackingList/detail.js
@@ -395,7 +395,7 @@ import React, {useState,
                                 }, 0);
                                 totalNettoWeight         = Math.round(totalNettoWeight         * 10) / 10;
 
-                                const totalSubtotalPrice = ListItem.reduce((sum, item) => {
+                                let totalSubtotalPrice = ListItem.reduce((sum, item) => {
                                     return sum + parseFloat(item.totalprice || 0);
                                 }, 0);
                                 totalSubtotalPrice         = Math.round(totalSubtotalPrice         * 10) / 10;
