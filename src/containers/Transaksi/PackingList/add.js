df164e9b3308dd46b90594b751b32cf7c2019830
donisimpoool <doni.ario@simpool.id>
2026-06-22 19:26:14 +0700

    no message

--- src/containers/Transaksi/PackingList/add.js
+++ src/containers/Transaksi/PackingList/add.js
@@ -989,7 +989,7 @@ export default function AddPackingList(props) {
                                                         }, 0);
                                                         totalNettoWeight         = Math.round(totalNettoWeight         * 10) / 10;
 
-                                                        const totalSubtotalPrice = ListItems.reduce((sum, item) => {
+                                                        let totalSubtotalPrice = ListItems.reduce((sum, item) => {
                                                             let val = item.subtotalprice ? item.subtotalprice : 0;
                                                             let valTemp = '';
                                                             if (new String(val).includes(',')) {
